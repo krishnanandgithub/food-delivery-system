b0c4b4868c9684915251ec80b3adefc642b8a3e5
Krishnanand <110751379+krishnanandgithub@users.noreply.github.com>
2025-01-31 15:56:59 +0530

    main.js modified

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-import { Order } from "./src/order.js";
+const foodPreparation = (time, order) => {
+  setTimeout(() => {
+    const timeTaken = (Date.now() - time) / 1000;
+    console.log([timeTaken], "Preparing food...");
+  }, 3000);
+};
+
+const orderReceived = (time) => {
+  setTimeout(() => {
+    const timeTaken = (Date.now() - time) / 1000;
+    const orderId = Math.floor(Math.random() * 899) + 100;
+    console.log([timeTaken], "Order received:", { orderId });
+    foodPreparation(time, {orderId});
+  }, 0);
+};
 
 const main = () => {
   const time = Date.now();
-  const orderId = Math.floor(Math.random() * 899) + 100;
-  const order = new Order(time, orderId);
-  order.receive();
+  orderReceived(time);
 };
 
 main();
