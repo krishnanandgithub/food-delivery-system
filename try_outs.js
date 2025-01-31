603bbb8d97119bcadbf180c6e0c8e8d73a9370d9
Krishnanand <110751379+krishnanandgithub@users.noreply.github.com>
2025-01-31 15:57:32 +0530

    try_outs.js modified

--- try_outs.js
+++ try_outs.js
@@ -1,22 +1,10 @@
-const foodPreparation = (time, order) => {
-  setTimeout(() => {
-    const timeTaken = (Date.now() - time) / 1000;
-    console.log([timeTaken], "Preparing food...");
-  }, 3000);
-};
-
-const orderReceived = (time) => {
-  setTimeout(() => {
-    const timeTaken = (Date.now() - time) / 1000;
-    const orderId = Math.floor(Math.random() * 899) + 100;
-    console.log([timeTaken], "Order received:", { orderId });
-    foodPreparation(time, {orderId});
-  }, 0);
-};
+import { Order } from "./src/order.js";
 
 const main = () => {
   const time = Date.now();
-  orderReceived(time);
+  const orderId = Math.floor(Math.random() * 899) + 100;
+  const order = new Order(time, orderId);
+  order.receive();
 };
 
 main();
